649df08bb13a3d1dd2babb28e1fbc943f4e9960f
Jubreel <odjubreel2@gmail.com>
2026-01-14 00:10:27 +0100

    adding api url to weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -12,6 +12,22 @@ import overcast_icon from '../assets/icon-overcast.webp'
 import cloudy_icon from '../assets/icon-partly-cloudy.webp'
 
 const Weather = () => {
+
+  const search = async(city)=> {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    }
+  }
+
+
+  useEffect(()=> {
+    search('Mumbai');
+  }, [])
+  
   return (
     <div className='weather'>
       <div className="search-bar">
